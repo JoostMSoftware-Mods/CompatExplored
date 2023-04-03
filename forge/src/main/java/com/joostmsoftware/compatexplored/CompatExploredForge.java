18f192dd807626caf7b94d88cba9d74fae611157
JoostMSoftware <joostmsoftware@gmail.com>
2023-04-03 23:18:42 +0200

    forgor this

--- forge/src/main/java/com/joostmsoftware/compatexplored/CompatExploredForge.java
+++ forge/src/main/java/com/joostmsoftware/compatexplored/CompatExploredForge.java
@@ -1,15 +1,11 @@
 package com.joostmsoftware.compatexplored;
 
-import dev.architectury.platform.forge.EventBuses;
 import com.joostmsoftware.CompatExplored.CompatExplored;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(CompatExplored.MOD_ID)
 public class CompatExploredForge {
     public CompatExploredForge() {
-        // Submit our event bus to let architectury register our content on the right time
-        EventBuses.registerModEventBus(CompatExplored.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
         CompatExplored.init();
     }
 }
